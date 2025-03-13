bb36f830a03557e806073ca843f3954e5181040c
ekachka33 <fetisova.eka007@gmail.com>
2025-03-13 12:08:48 +0700

    revision

--- catalog/views.py
+++ catalog/views.py
@@ -6,7 +6,8 @@ from django.core.paginator import Paginator
 
 def home(request):
     latest_products = Product.objects.order_by('-created_at')[:5]  # Получаем последние 5 продуктов
-    return render(request, 'home.html', {'latest_products': latest_products})
+    return render(request, 'catalog/home.html', {'latest_products': latest_products})
+
 def contacts(request):
     if request.method == "POST":
         name = request.POST.get("name")
@@ -17,27 +18,28 @@ def contacts(request):
 
         # Сообщение об успешной отправке
         messages.success(request, f"Спасибо, {name}! Ваше сообщение отправлено.")
-        return render(request, "contacts.html")
+        return redirect('catalog:contacts')  # Перенаправление на страницу контактов
 
-    return render(request, "contacts.html")
+    return render(request, "catalog/contacts.html")
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)  # Получаем продукт по первичному ключу
-    return render(request, 'product_detail.html', {'product': product})
+    return render(request, 'catalog/product_detail.html', {'product': product})
 
 def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Товар успешно добавлен!")  # Сообщение об успешном добавлении
             return redirect('catalog:product_list')  # Перенаправление на список товаров
     else:
         form = ProductForm()
-    return render(request, 'add_product.html', {'form': form})
+    return render(request, 'catalog/add_product.html', {'form': form})
 
 def product_list(request):
     products = Product.objects.all()
     paginator = Paginator(products, 10)  # Показывать 10 товаров на странице
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'product_list.html', {'page_obj': page_obj})
+    return render(request, 'catalog/product_list.html', {'page_obj': page_obj})
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,9 +74,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-from decouple import config
-
 DATABASES = {
     'default': {
         'ENGINE': config('DB_ENGINE'),
